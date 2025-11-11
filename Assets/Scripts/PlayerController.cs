6c2a2f47a3ba1214be01dd9dd54c6dab437d1341
videoJames23 <jjrice429@gmail.com>
2025-11-11 14:28:32 +0000

    test commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,7 +21,7 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //test
+        
     }
 
     // Update is called once per frame
