1a6b10be90560864673bf2976c02b94ed14cea94
Fred-qub <fwilliamson01@qub.ac.uk>
2026-02-04 22:25:57 +0000

    got rid of the magic numbers controlling the true player and enemy stats

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,6 +20,26 @@ public class GameManager : MonoBehaviour
 
     private Vector2 vPlayerVelocity;
     private Vector2 vEnemyVelocity;
+
+    //Removing magic numbers using predetermined values for each stat level -F
+    [SerializeField] private float playerSpeedLVL0 = 0f;
+    [SerializeField] private float playerSpeedLVL1 = 3f;
+    [SerializeField] private float playerSpeedLVL2 = 7f;
+    [SerializeField] private float playerSpeedLVL3 = 10f;
+    
+    [SerializeField] private float playerJumpLVL0 = 0f;
+    [SerializeField] private float playerJumpLVL1 = 5f;
+    [SerializeField] private float playerJumpLVL2 = 7f;
+    [SerializeField] private float playerJumpLVL3 = 9f;
+
+    [SerializeField] private float enemySpeedLVL0 = 0f;
+    [SerializeField] private float enemySpeedLVL1 = 3f;
+    [SerializeField] private float enemySpeedLVL2 = 7f;
+    [SerializeField] private float enemySpeedLVL3 = 10f;
+    
+    [SerializeField] private float enemySizeLVL1 = 1.5f;
+    [SerializeField] private float enemySizeLVL2 = 3f;
+    [SerializeField] private float enemySizeLVL3 = 4.5f;
     
     void Start()
     {
@@ -119,14 +139,14 @@ public class GameManager : MonoBehaviour
             playerController.bInMenu = true;
         }
         
-        else if (!playerController.bInMenuP && !playerController.bInMenuE) //redundant condition -F
+        else //if (!playerController.bInMenuP && !playerController.bInMenuE) redundant condition -F
         {
             playerController.bInMenu = false;
         }
         statBlockUI.UpdateUI();
     }
 
-    //Pausing in general could be implemented better and this is just part of that -F
+    //Pausing in general could be implemented better, getting rid of all of these if/else statements -F
     void MenuFreezeToggle()
     {
         if (playerController.bInMenu)
@@ -172,10 +192,10 @@ public class GameManager : MonoBehaviour
             switch (statBlockUI.statsP[1]) // player speeds
             {
                 //MAGIC NUMBERS RAAAAAAAAAAAAAAAAH I HATE MAGIC NUMBERS -F
-                case 0: playerController.fPlayerSpeed = 0; break;
-                case 1: playerController.fPlayerSpeed = 3; break;
-                case 2: playerController.fPlayerSpeed = 7; break;
-                case 3: playerController.fPlayerSpeed = 10; break;
+                case 0: playerController.fPlayerSpeed = playerSpeedLVL0; break;
+                case 1: playerController.fPlayerSpeed = playerSpeedLVL1; break;
+                case 2: playerController.fPlayerSpeed = playerSpeedLVL2; break;
+                case 3: playerController.fPlayerSpeed = playerSpeedLVL3; break;
             }
         }
     }
@@ -186,10 +206,10 @@ public class GameManager : MonoBehaviour
             switch (statBlockUI.statsP[2]) //player jump heights
             {
                 //MAGIC NUMBERS RAAAAAAAAAAAAAAAAH I HATE MAGIC NUMBERS -F
-                case 0: playerController.fPlayerJump = 0; break;
-                case 1: playerController.fPlayerJump = 5; break;
-                case 2: playerController.fPlayerJump = 7; break;
-                case 3: playerController.fPlayerJump = 9; break;
+                case 0: playerController.fPlayerJump = playerJumpLVL0; break;
+                case 1: playerController.fPlayerJump = playerJumpLVL1; break;
+                case 2: playerController.fPlayerJump = playerJumpLVL2; break;
+                case 3: playerController.fPlayerJump = playerJumpLVL3; break;
             }
         }
     }
@@ -208,10 +228,11 @@ public class GameManager : MonoBehaviour
         {
             switch (statBlockUI.statsE[1]) // enemy speeds
             {
-                case 0: enemyController.fEnemySpeed = 0; break;
-                case 1: enemyController.fEnemySpeed = 3; break;
-                case 2: enemyController.fEnemySpeed = 7; break;
-                case 3: enemyController.fEnemySpeed = 10; break;
+                //MAGIC NUMBERS RAAAAAAAAAAAAAAAAH I HATE MAGIC NUMBERS -F
+                case 0: enemyController.fEnemySpeed = enemySpeedLVL0; break;
+                case 1: enemyController.fEnemySpeed = enemySpeedLVL1; break;
+                case 2: enemyController.fEnemySpeed = enemySpeedLVL2; break;
+                case 3: enemyController.fEnemySpeed = enemySpeedLVL3; break;
             }
         }
     }
@@ -222,8 +243,11 @@ public class GameManager : MonoBehaviour
             switch (statBlockUI.statsE[2]) //enemy sizes
             {
                 // If enemy grows into wall, movement stops
+                
+                //I'm assuming it's dependent on the differences between sizes, but the Y offset floats are more magic numbers,
+                //which could be replaced with a calculation which works regardless of what you set the sizes to -F
                 case 1:
-                    enemyController.fEnemySize = 1.5f;
+                    enemyController.fEnemySize = enemySizeLVL1;
                     if (statBlockUI.iPrevSize != 1)
                     {
                         enemyRb.position = new Vector2(enemyRb.position.x, enemyRb.position.y - 0.81f);
@@ -236,10 +260,10 @@ public class GameManager : MonoBehaviour
                     {
                         case 1:
                             enemyRb.position = new Vector2(enemyRb.position.x, enemyRb.position.y + 0.81f);
-                            enemyController.fEnemySize = 3f;
+                            enemyController.fEnemySize = enemySizeLVL2;
                             break;
                         case 3:
-                            enemyController.fEnemySize = 3f;
+                            enemyController.fEnemySize = enemySizeLVL2;
                             enemyRb.position = new Vector2(enemyRb.position.x, enemyRb.position.y - 0.726443f);
                             break;
                     }
@@ -249,7 +273,7 @@ public class GameManager : MonoBehaviour
                     if (statBlockUI.iPrevSize != 3)
                     {
                         enemyRb.position = new Vector2(enemyRb.position.x, enemyRb.position.y + 0.726443f);
-                        enemyController.fEnemySize = 4.5f;
+                        enemyController.fEnemySize = enemySizeLVL3;
                     }
 
                     break;
