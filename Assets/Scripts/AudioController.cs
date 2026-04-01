a9b0a5e065a39e012ca2f7d942ddf936887e199a
videoJames23 <jjrice429@gmail.com>
2026-04-01 19:05:39 +0100

    AudioController Cleanup
    Comments added to space out subscriptions, play logic centralised for simplicity

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -14,95 +14,90 @@ public class AudioController : MonoBehaviour
     [SerializeField] private AudioSource errorSource;
 
     private void OnEnable()
-
     {
-        StatBlockUI.OnMenuOpen += PlayOpen;
-        StatBlockUI.OnMenuClose += PlayClose;
-        StatBlockChangesP.OnError += PlayError;
-        StatBlockChangesE.OnError += PlayError;
-        StatBlockChangesP.OnUp += PlayUp;
-        StatBlockChangesP.OnDown += PlayDown;
-        StatBlockChangesE.OnUp += PlayUp;
-        StatBlockChangesE.OnDown += PlayDown;
-        StatBlockInput.OnIndexChanged += PlayIndex;
-        
+        // Start screen
         MainMenuScreen.OnButtonClicked += PlayUp;
         LevelSelectScreen.OnBack += PlayDown;
         CreditsScreen.OnBack += PlayDown;
         
+        // StatBlock open/close
+        StatBlockUI.OnMenuOpen += PlayOpen;
+        StatBlockUI.OnMenuClose += PlayClose;
+        
+        // StatBlock error
+        StatBlockChangesP.OnError += PlayError;
+        StatBlockChangesE.OnError += PlayError;
+        
+        // Stat changes
+        StatBlockChangesP.OnUp += PlayUp;
+        StatBlockChangesP.OnDown += PlayDown;
+        StatBlockChangesE.OnUp += PlayUp;
+        StatBlockChangesE.OnDown += PlayDown;
+        
+        // Stat selection
+        StatBlockInput.OnIndexChanged += PlayIndex;
+        
+        // Player sfx
         PlayerMovement.OnJumpEvent += PlayJump;
         PlayerDamage.OnDamage += PlayDamage;
         PlayerCollisions.OnCompletion += PlayCompletion;
         
+        // Enemy sfx
         EnemyDamage.OnDamage += PlayDamage;
     }
 
     private void OnDisable()
     {
-        StatBlockUI.OnMenuOpen -= PlayOpen;
-        StatBlockUI.OnMenuClose -= PlayClose;
-        StatBlockChangesP.OnError -= PlayError;
-        StatBlockChangesE.OnError -= PlayError;
-        StatBlockChangesP.OnUp -= PlayUp;
-        StatBlockChangesP.OnDown -= PlayDown;
-        StatBlockChangesE.OnUp -= PlayUp;
-        StatBlockChangesE.OnDown -= PlayDown;
-        StatBlockInput.OnIndexChanged -= PlayIndex;
-        
+        // Start screen
         MainMenuScreen.OnButtonClicked -= PlayUp;
         LevelSelectScreen.OnBack -= PlayDown;
         CreditsScreen.OnBack -= PlayDown;
         
+        // StatBlock open/close
+        StatBlockUI.OnMenuOpen -= PlayOpen;
+        StatBlockUI.OnMenuClose -= PlayClose;
+        
+        // StatBlock error
+        StatBlockChangesP.OnError -= PlayError;
+        StatBlockChangesE.OnError -= PlayError;
+        
+        // Stat changes
+        StatBlockChangesP.OnUp -= PlayUp;
+        StatBlockChangesP.OnDown -= PlayDown;
+        StatBlockChangesE.OnUp -= PlayUp;
+        StatBlockChangesE.OnDown -= PlayDown;
+        
+        // Stat selection
+        StatBlockInput.OnIndexChanged -= PlayIndex;
+        
+        // Player sfx
         PlayerMovement.OnJumpEvent -= PlayJump;
         PlayerDamage.OnDamage -= PlayDamage;
         PlayerCollisions.OnCompletion -= PlayCompletion;
         
+        // Enemy sfx
         EnemyDamage.OnDamage -= PlayDamage;
     }
     
-
-    private void PlayOpen()
+    
+    private void Play(AudioSource source)
     {
-        openSource.Play();
-    }
+        if (!source)
+            return;
 
-    private void PlayClose()
-    {
-        closeSource.Play();
-    }
+        if (!source.isActiveAndEnabled)
+            return;
 
-    private void PlayDamage()
-    {
-        damageSource.Play();
+        source.Play();
     }
+    private void PlayOpen() => Play(openSource);
+    private void PlayClose() => Play(closeSource);
+    private void PlayDamage() => Play(damageSource);
+    private void PlayUp() => Play(upSource);
+    private void PlayDown() => Play(downSource);
+    private void PlayIndex() => Play(indexSource);
+    private void PlayError() => Play(errorSource);
+    private void PlayJump() => Play(jumpSource);
+    private void PlayCompletion() => Play(completionSource);
 
-    private void PlayUp()
-    {
-        upSource.Play();
-    }
-
-    private void PlayDown()
-    {
-        downSource.Play();
-    }
-
-    private void PlayIndex()
-    {
-        indexSource.Play();
-    }
-
-    private void PlayError()
-    {
-        errorSource.Play();
-    }
-
-    private void PlayJump()
-    {
-        jumpSource.Play();
-    }
-
-    private void PlayCompletion()
-    {
-        completionSource.Play();
-    }
 }
