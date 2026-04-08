25931581747d6ddff53355b3972d1224475c9242
videoJames23 <jjrice429@gmail.com>
2026-04-08 13:34:26 +0100

    level 7 layout tweak

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -27,7 +27,7 @@ public class GameManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    private void Update()
+    private void Update() 
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
